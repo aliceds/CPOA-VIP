d1987434fa4d4eea9395453b949167c492ca89ea
aliceds <alice.desforges@live.fr>
2017-05-15 12:07:04 +0200

    1er commit

---
+++ src/cpoa/vip/CPOAVIP.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpoa.vip;
+
+/**
+ *
+ * @author Alice
+ */
+public class CPOAVIP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
